f1e5ec6a6c27303a84d0069869fc42b662dcf498
lichenji <1714815278@qq.com>
2019-12-29 21:20:32 +0800

    扩展已有类型

--- TestModels/entry/Meaages.go
+++ TestModels/entry/Meaages.go
@@ -20,12 +20,22 @@ func (root *MyTree) PostOrder() {
 	root.Print()
 }
 
+//  表面上 看起来 是重载，其实只是魔法函数，并不是是重载
+func (root *MyTree) Travels() {
+	fmt.Println("This is MyTree Travels")
+}
+
 func main() {
+	//内嵌结构赋值
 	root := MyTree{&Nodes.Tree{Value: 200}}
 	root.Left = Nodes.CreateTree(50)
 	root.Right = Nodes.CreateTree(300)
 	root.Travels()
+	root.Tree.Travels()
 	fmt.Println()
 	root.PostOrder()
 
+	//说明 不是重载
+	//var BaseTree *root.Tree
+	//BaseTree = root
 }
