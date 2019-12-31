9c5cd9986974806ff166cd0dc47778ad6d16c55f
lichenji <lichenji@qianxin.com>
2019-12-31 11:17:05 +0800

    接口组合

--- 06/Interfaces/frawsss/fraws.go
+++ 06/Interfaces/frawsss/fraws.go
@@ -4,6 +4,10 @@ type INters struct {
 	Contens string
 }
 
-func (root INters) Get(url string) string {
+func (root *INters) Get(url string) string {
 	return root.Contens
+}
+
+func (root *INters) Post(url string, contents map[string]string) {
+	root.Contens = contents["names"]
 }
